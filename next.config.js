9a1ed5affc38398f5b679fbd47dea21b6bfa92ec
Alexis Pavon <themaster034@gmail.com>
2022-11-18 13:17:46 -0300

    config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['cdn.shopify.com', 'capricathemes.com', 'instagram.fcnq2-1.fna.fbcdn.net', 'instagram.fcnq2-2.fna.fbcdn.net', 'res.cloudinary.com'],
+  },
 }
 
 module.exports = nextConfig
