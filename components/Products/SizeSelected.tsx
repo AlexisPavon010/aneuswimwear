851665db78ce733321ed5c934a357ea487744420
Alexis Pavon <themaster034@gmail.com>
2022-11-18 13:45:20 -0300

    add select size

---
+++ components/Products/SizeSelected.tsx
@@ -0,0 +1,29 @@
+import { Button, Stack } from "@chakra-ui/react"
+
+interface SizeSelectedProps {
+  sizes: any;
+  selectedSize?: string;
+  onSelectedSize: (value: string) => void
+}
+
+export const SizeSelected = ({ sizes, selectedSize, onSelectedSize }: SizeSelectedProps) => {
+
+  return (
+    <Stack m='10px 0' spacing={1} direction='row' align='center'>
+      {sizes.map((size: string, i: number) => (
+        <Button
+          key={i}
+          p={0}
+          fontSize='13px'
+          fontWeight={600}
+          rounded='full'
+          variant={'solid'}
+          colorScheme={selectedSize === size ? 'blue' : 'gray'}
+          onClick={() => onSelectedSize(size)}
+        >
+          {size}
+        </Button>
+      ))}
+    </Stack>
+  )
+}
