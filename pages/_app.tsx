62f890c28512cfa261815a6f35556b288046c5b9
Alexis Pavon <themaster034@gmail.com>
2022-11-18 13:32:39 -0300

    add providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,32 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { SessionProvider } from 'next-auth/react'
+import { Provider } from 'react-redux';
+import { SWRConfig } from 'swr'
+import 'swiper/css/effect-fade';
+import "swiper/css/navigation";
+import 'swiper/css';
 
 import '../styles/globals.css'
-import { SessionProvider } from 'next-auth/react'
+import { store } from '../redux/store';
+import { Layout } from '../components/Layout';
 
-export default function App({ Component, pageProps, session }: any) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <SessionProvider session={pageProps.session}>
+        <Provider store={store}>
+          <ChakraProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ChakraProvider>
+        </Provider>
+      </SessionProvider>
+    </SWRConfig>
   )
 }
