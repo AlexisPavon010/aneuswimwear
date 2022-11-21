076e63e9ebeeb261efa98773c8fc7a70b72a1e03
Alexis Pavon <themaster034@gmail.com>
2022-11-21 19:43:30 -0300

    add home props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 import { Box } from '@chakra-ui/react'
 
 import { Banner } from '../components/Banner'
@@ -6,17 +7,25 @@ import { SlideProducts } from '../components/SlideProducts'
 import { Categories } from '../components/Categories'
 import { InstaFeed } from '../components/InstaFeed'
 import { WhatsAppButton } from '../components/WhatsAppButton'
-import { IProduct } from '../interfaces'
+import { IBanner, INewsletter, IProduct } from '../interfaces'
 import { GetServerSideProps } from 'next'
 import { dbProducts } from '../database'
-import { Newsletter } from '../components/Newsletter'
+import { getBanner } from '../database/dbBanner'
+import { getNewsletter } from '../database/dbNewsletter'
 
 interface HomeProps {
   news: IProduct[];
+  banner: IBanner;
+  newsletter: INewsletter;
   best_sellers: IProduct[];
 }
 
-export default function Home({ news, best_sellers }: HomeProps) {
+export default function Home({ news, best_sellers, banner, newsletter }: HomeProps) {
+
+  const Newsletter = dynamic(() =>
+    import('../components/Newsletter').then((mod) => mod.Newsletter)
+  )
+
   return (
     <>
       <Head>
@@ -29,13 +38,13 @@ export default function Home({ news, best_sellers }: HomeProps) {
       </Head>
 
       <Box as='main'>
-        <Banner />
+        <Banner banner={banner} />
         <SlideProducts title='New arrivals' products={news} />
         <Categories />
         <SlideProducts title='Best sellers' products={best_sellers} />
         <InstaFeed />
         <WhatsAppButton />
-        <Newsletter />
+        <Newsletter newsletter={newsletter} />
       </Box>
     </>
   )
@@ -43,12 +52,16 @@ export default function Home({ news, best_sellers }: HomeProps) {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
 
+  const banner = await getBanner()
+  const newsletter = await getNewsletter()
   const news = await dbProducts.getAllProductByGender('new')
   const best_sellers = await dbProducts.getAllProductByGender('best_sellers')
 
   return {
     props: {
       news,
+      banner,
+      newsletter,
       best_sellers
     }
   }
