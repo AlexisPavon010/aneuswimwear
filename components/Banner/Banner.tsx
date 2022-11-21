11b14434f03644a1f6eb35f66efdd11b055bd191
Alexis Pavon <themaster034@gmail.com>
2022-11-21 19:42:29 -0300

    add dinamyc banner

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,10 +1,18 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay } from "swiper";
-import Image from 'next/image';
+import NextImage from 'next/image';
+import NextLink from 'next/link';
 
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 
-export const Banner = () => {
+interface Banner {
+  title: string;
+  subtitle: string;
+  images: string[];
+}
+
+export const Banner = ({ banner }: { banner: Banner }) => {
+
   return (
     <Swiper
       spaceBetween={30}
@@ -18,16 +26,78 @@ export const Banner = () => {
       }}
       modules={[EffectFade, Autoplay]}
     >
-      <SwiperSlide>
-        <Box>
-          <Image src='https://capricathemes.com/prestashop/PRS12/PRS120296/modules/cz_imageslider/views/img/sample-2.jpg' alt='banner' width={6000} height={2500} />
-        </Box>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Box>
-          <Image src='https://capricathemes.com/prestashop/PRS12/PRS120296/modules/cz_imageslider/views/img/sample-1.jpg' alt='banner' width={6000} height={2500} />
-        </Box>
-      </SwiperSlide>
+      {
+        banner.images.map((url: string, i: number) => (
+          <SwiperSlide key={i}>
+            <Box position='relative' >
+              <Box
+                position='absolute'
+                height='100%'
+                width='100%'
+                left='0'
+                top='0'
+              >
+                <NextImage
+                  style={{
+                    objectFit: 'cover',
+                    position: 'absolute',
+                    height: '100%',
+                    width: '100%',
+                    left: '0',
+                    top: '0',
+                  }}
+                  width={6000}
+                  height={6000}
+                  src={url}
+                  alt='banner'
+                />
+              </Box>
+              <Flex
+                justify='center'
+                align='center'
+                minH='90vh'
+              >
+                <Box
+                  p='30px'
+                  textAlign='center'
+                  zIndex={1}
+                >
+                  <Text
+                    mb={4}
+                    color='white'
+                    fontSize='40px'
+                    fontWeight={600}
+                  >
+                    {banner.title.toUpperCase()}
+                  </Text>
+                  <Text
+                    mb={4}
+                    color='white'
+                    fontSize='24px'
+                  >
+                    {banner.subtitle.toUpperCase()}
+                  </Text>
+                  <Button
+                    as={NextLink}
+                    href='/collections/all'
+                    variant='outline'
+                    color='white'
+                    borderRadius={0}
+                    _hover={{}}
+                    fontSize='14px'
+                    fontWeight={400}
+                    height='43px'
+                    width='150px'
+                  >
+                    SHOP NOW
+                  </Button>
+                </Box>
+              </Flex>
+            </Box>
+          </SwiperSlide>
+        ))
+      }
     </Swiper>
+
   )
 }
