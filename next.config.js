7380d3c02182771f38ea5b303f93730cbe081480
Alexis Pavon <themaster034@gmail.com>
2022-11-21 00:56:09 -0300

    add images urls

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn.shopify.com', 'capricathemes.com', 'instagram.fcnq2-1.fna.fbcdn.net', 'instagram.fcnq2-2.fna.fbcdn.net', 'res.cloudinary.com'],
+    domains: [
+      'cdn.shopify.com',
+      'capricathemes.com',
+      'res.cloudinary.com',
+      'instagram.fcnq2-1.fna.fbcdn.net',
+      'instagram.fcnq2-2.fna.fbcdn.net',
+      'd3k81ch9hvuctc.cloudfront.net'
+    ],
   },
 }
 
