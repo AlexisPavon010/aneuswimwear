4354ca2a7d7826c880e47bb9e1b1b6ad7a9a791c
Alexis Pavon <themaster034@gmail.com>
2022-11-24 17:24:31 -0300

    add checkout page

---
+++ pages/checkout/index.tsx
@@ -0,0 +1,70 @@
+import { GetServerSideProps } from 'next'
+import axios from 'axios'
+
+import { Order } from '../../models'
+import { db } from '../../database'
+import { Box, Drawer, DrawerOverlay, Flex, Spinner, Text } from '@chakra-ui/react'
+
+const OrderCompleted = () => {
+  return (
+    <Box height='100vh'>
+      <Drawer
+        isOpen={true}
+        onClose={() => { }}>
+        <DrawerOverlay
+          flexDirection='column'
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Spinner mb='25px' size='xl' color='blue.500' thickness='4px' speed='0.65s' />
+          <Text>Comprobrando Transaccion</Text>
+        </DrawerOverlay>
+      </Drawer>
+    </Box>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { id, clientTransactionId: clientTxId } = query as { id: string, clientTransactionId: string }
+  
+  const token = process.env.NEXT_PUBLIC_PAYPHONE_TOKEN
+
+  try {
+    await db.connect()
+    const { data } = await axios.post('https://pay.payphonetodoesposible.com/api/button/V2/Confirm', {
+      id,
+      clientTxId
+    }, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    const dbOrder = await Order.findById(clientTxId)
+
+    if (data.transactionStatus === 'Approved') {
+      dbOrder.transactionId = id
+      dbOrder.isPaid = true
+      await dbOrder.save()
+    }
+
+    return {
+      redirect: {
+        destination: `/order/${clientTxId}`,
+        permanent: false
+      }
+    }
+
+  } catch (error) {
+    console.log(error)
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+}
+
+export default OrderCompleted;
