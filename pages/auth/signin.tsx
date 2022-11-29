1267b1593f30e45e7bfd854fe944bc1f8b6897a0
Alexis Pavon <themaster034@gmail.com>
2022-11-29 19:08:48 -0300

    add recovery password

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Flex, FormControl, FormErrorMessage, Image, Input, Stack, Text } from '@chakra-ui/react'
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, Divider, Flex, FormControl, FormErrorMessage, Image, Input, Link as ChakraLink, Stack, Text, useToast } from '@chakra-ui/react'
 import { FaFacebookF, FaArrowLeft } from 'react-icons/fa'
 import { FcGoogle } from 'react-icons/fc'
 import { useForm } from "react-hook-form";
@@ -7,22 +7,21 @@ import { getProviders, getSession, signIn as LoginWhitProviders, } from 'next-au
 import { GetServerSideProps } from 'next';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
-import Link from 'next/link';
+import NextLink from 'next/link';
 import Head from 'next/head';
 
-import { registerUser } from '../../client';
+import { recoveryPassword, registerUser } from '../../client';
 
 const Signin = ({ providers, error }: any) => {
   const [isRegister, setIsRegister] = useState(false)
-
-  console.log(providers)
+  const [isForgot, setIsForgot] = useState(false)
 
   return (
     <Box height='100vh'>
       <Head>
         <title>Aneuswimwear | {isRegister ? 'Register' : 'Signin'}</title>
       </Head>
-      <Flex height='100%' >
+      <Flex height='100%'>
         <Flex display={{ base: 'none', lg: 'block' }} bgImage={'/assets/login.jpg'} position='relative' flex='1'>
           <Box
             position='absolute'
@@ -32,9 +31,9 @@ const Signin = ({ providers, error }: any) => {
             zIndex='10'
             color='#fff'
           >
-            <Link href='/'>
+            <NextLink href='/'>
               <FaArrowLeft size='24px' />
-            </Link>
+            </NextLink>
           </Box>
           <Flex
             backgroundColor='rgba(0,0,0,.6)'
@@ -66,7 +65,7 @@ const Signin = ({ providers, error }: any) => {
         </Flex>
         <Flex padding='32px' flex='1' alignItems='center' justifyContent='center'>
           <Box width='100%' maxW='450px'>
-            <Flex mb={8} justifyContent='center'>
+            <Flex m={{ base: '40px 0 32px 0', lg: '0 0 32px 0' }} mb={8} justifyContent='center'>
               <Image w='150px' src='/assets/logo_negro.png' />
             </Flex>
             <Text
@@ -76,73 +75,82 @@ const Signin = ({ providers, error }: any) => {
               fontWeight={900}
               fontSize='27px'
             >
-              Join Aneus Now
+              Join The Aneu Club
             </Text>
-            <Box>
-              <Stack direction='column' spacing={4}>
-                {providers ? Object.values(providers).map((provider: any) => {
-                  if (provider.id === 'credentials') return null
-                  console.log(providers)
-                  let icon;
-                  let color;
-                  let variant;
-                  switch (provider.id) {
-                    case 'facebook':
-                      icon = <FaFacebookF />
-                      color = 'facebook'
-                      break;
-                    case 'google':
-                      icon = <FcGoogle />
-                      break;
+            {isForgot ? (
+              <ForgotComponent setIsForgot={setIsForgot} />
+            ) : (
+              <>
+                <Box>
+                  <Stack direction='column' spacing={4}>
+                    {providers ? Object.values(providers).map((provider: any) => {
+                      if (provider.id === 'credentials') return null
+                      console.log(providers)
+                      let icon;
+                      let color;
+                      let variant;
+                      switch (provider.id) {
+                        case 'facebook':
+                          icon = <FaFacebookF />
+                          color = 'facebook'
+                          break;
+                        case 'google':
+                          icon = <FcGoogle />
+                          break;
 
-                    default:
-                      break;
-                  }
-                  return (
-                    <div key={provider.name}>
-                      <Button
-                        onClick={() => LoginWhitProviders(provider.id)}
-                        leftIcon={icon}
-                        colorScheme={color}
-                        width='100%'
-                      >
-                        Sign in with {provider.name}
-                      </Button>
-                    </div>
-                  )
-                }) : null}
-              </Stack>
-              <Flex alignItems='center' margin='16px 0'>
-                <Divider />
-                <Text fontSize='14px' color='gray' w='100%' align='center'>
-                  or via email
-                </Text>
-                <Divider />
-              </Flex>
-              {isRegister ? (
-                <RegisterComponent />
-              ) : (
-                <LoginComponent />
-              )}
-            </Box>
-            <Box padding='16px' textAlign='center'>
-              <Flex justifyContent='center'>
-                <Text
-                  mr={2}
-                >
-                  Have an account?
-                </Text>
-                <Text
-                  fontWeight={900}
-                  onClick={() => setIsRegister((state: boolean) => !state)}
-                >
-                  {isRegister ? 'Login' : 'Sign in'}
-                </Text>
-              </Flex>
-            </Box>
+                        default:
+                          break;
+                      }
+                      return (
+                        <div key={provider.name}>
+                          <Button
+                            onClick={() => LoginWhitProviders(provider.id)}
+                            leftIcon={icon}
+                            colorScheme={color}
+                            width='100%'
+                          >
+                            Sign in with {provider.name}
+                          </Button>
+                        </div>
+                      )
+                    }) : null}
+                  </Stack>
+                  <Flex alignItems='center' margin='16px 0'>
+                    <Divider />
+                    <Text fontSize='14px' color='gray' w='100%' align='center'>
+                      or via email
+                    </Text>
+                    <Divider />
+                  </Flex>
+                  {isRegister ? (
+                    <RegisterComponent />
+                  ) : (
+                    <LoginComponent />
+                  )}
+                </Box>
+                <Box padding='16px' textAlign='center'>
+                  <Flex mb={4} justifyContent='center'>
+                    <Text
+                      mr={2}
+                    >
+                      Have an account?
+                    </Text>
+                    <Text
+                      fontWeight={900}
+                      onClick={() => setIsRegister((state: boolean) => !state)}
+                    >
+                      {isRegister ? 'Login' : 'Sign in'}
+                    </Text>
+                  </Flex>
+                  <ChakraLink onClick={() => setIsForgot(true)}>
+                    Forgot password?
+                  </ChakraLink>
+                </Box>
+              </>
+            )}
           </Box>
-        </Flex>
-      </Flex>
+        </Flex >
+      </Flex >
     </Box >
   )
 }
@@ -159,7 +167,6 @@ const LoginComponent = () => {
   });
 
   const handleLoginWhitEmailAndPassword = async ({ email, password }: any) => {
-    console.log('first')
     await LoginWhitProviders('credentials', { email, password })
   }
 
@@ -197,7 +204,6 @@ const RegisterComponent = () => {
 
   const handleRegisterWhitEmailAndPassword = async ({ email, password, name }: { email: string, password: string, name: string }) => {
 
-    console.log({ email, password, name })
     try {
       const { data } = await registerUser(email, password, name)
       await LoginWhitProviders('credentials', { email, password })
@@ -229,6 +235,96 @@ const RegisterComponent = () => {
         <Input {...register("password")} mb='16px' type='password' name='password' placeholder='Password' focusBorderColor='none' />
       </FormControl>
       <Button onClick={handleSubmit(handleRegisterWhitEmailAndPassword)} margin='16px 0' background='#000' color='white' width='100%'>Register</Button>
+    </>
+  )
+}
+
+const ForgotComponent = ({ setIsForgot }: { setIsForgot: any }) => {
+  const [errorMessages, setErrorMessage] = useState('')
+  const [hasError, setHasError] = useState(false)
+  const [hasSuccess, setHasSuccess] = useState(false)
+  const [isLoading, setLoading] = useState(false)
+
+
+  const schema = yup.object({
+    email: yup.string().email().required(),
+  })
+
+  const { register, handleSubmit, watch, formState: { errors } }: any = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const handleRecoveryPassword = async ({ email }: { email: string }) => {
+    setLoading(true)
+    try {
+      const { data } = await recoveryPassword(email)
+      setHasSuccess(true)
+    } catch (error: any) {
+      console.log(error)
+      setErrorMessage(error.response.data.message)
+      setHasError(true)
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <>
+      {hasSuccess ? (
+        <Alert
+          status='success'
+          variant='subtle'
+          flexDirection='column'
+          alignItems='center'
+          justifyContent='center'
+          textAlign='center'
+          height='200px'
+        >
+          <AlertIcon boxSize='40px' mr={0} />
+          <AlertTitle mt={4} mb={1} fontSize='lg'>
+            Recovery confirmation!
+          </AlertTitle>
+          <AlertDescription maxWidth='sm'>
+            In your email you will receive a link to reset your password
+          </AlertDescription>
+        </Alert>
+      ) : (
+        <>
+          <FormControl isInvalid={hasError}>
+            <FormErrorMessage justifyContent='center'>{errorMessages}</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.email}>
+            <FormErrorMessage>{errors?.email?.message}</FormErrorMessage>
+            <Input {...register("email")} mb='16px' type='email' placeholder='Email' focusBorderColor='none' />
+          </FormControl>
+          <Flex justifyContent='space-between'>
+            <Button
+              onClick={() => setIsForgot(false)}
+              margin='16px 0'
+              color='#333'
+              borderColor='#333'
+              variant='outline'
+              _hover={{
+                color: 'white',
+                background: '#333'
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={handleSubmit(handleRecoveryPassword)}
+              margin='16px 0'
+              background='#d9534f'
+              color='white'
+              _hover={{}}
+              isLoading={isLoading}
+            >
+              Reset password
+            </Button>
+          </Flex>
+        </>
+      )}
     </>
   )
 }
