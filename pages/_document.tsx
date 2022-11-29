3d4e3722dbd1addd946515e98ade3ab6da27ff4b
Alexis Pavon <themaster034@gmail.com>
2022-11-29 17:20:11 -0300

    add favicon head

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/assets/test.jpg" />
+      </Head>
+      <body>
+        <NextScript />
+        <Main />
+      </body>
+    </Html>
+  )
+}
