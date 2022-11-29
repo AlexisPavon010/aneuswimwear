a8df039b420044c56b977f0c6962b38ef8be4cef
Alexis Pavon <themaster034@gmail.com>
2022-11-29 17:18:51 -0300

    add newsletter send email client

--- client/newsletter.ts
+++ client/newsletter.ts
@@ -6,9 +6,15 @@ interface Payload {
   image: string[];
   secondTitle: string;
   subtitle: string;
-  _id:string;
+  _id: string;
 }
 
 export const uploadNewsletter = (payload: Payload) => {
   return axios.post('/api/admin/newsletter', payload)
+}
+
+export const sendNewsletterEmail = (email: string) => {
+  return axios.post('/api/email/newsletter', {
+    email
+  })
 }
