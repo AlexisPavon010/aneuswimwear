e3c211ce34509634e80df2d81f1daa00ff1e14b3
Alexis Pavon <themaster034@gmail.com>
2022-12-01 13:43:09 -0300

    add tooltip

--- components/WhatsAppButton/WhatsAppButton.tsx
+++ components/WhatsAppButton/WhatsAppButton.tsx
@@ -1,22 +1,25 @@
-import { Button } from "@chakra-ui/react"
+import { Button, Tooltip } from "@chakra-ui/react"
 import { BsWhatsapp } from "react-icons/bs"
 
 export const WhatsAppButton = () => {
   return (
-    <Button
-      height='56px'
-      bottom='10px'
-      right='10px'
-      color='white'
-      backgroundColor='#4dc247'
-      boxShadow='2px 2px 6px rgb(0 0 0 / 40 %)'
-      borderRadius='50%'
-      position='fixed'
-      rounded='full'
-      zIndex={3}
-      _hover={{}}
-    >
-      <BsWhatsapp size='24px' />
-    </Button >
+    <Tooltip hasArrow label='Need Help?' placement='left'>
+      <Button
+        height='56px'
+        bottom='10px'
+        right='10px'
+        color='white'
+        backgroundColor='#4dc247'
+        boxShadow='2px 2px 6px rgb(0 0 0 / 40 %)'
+        borderRadius='50%'
+        position='fixed'
+        rounded='full'
+        zIndex={3}
+        _hover={{}}
+        onClick={() => window.open('https://api.whatsapp.com/send/?phone=543751307791&text=Hi+%2AAneu%2A%21+I+need+more+info+about+Homepage+https%3A%2F%2Faneuswimwear.vercel.app&type=phone_number&app_absent=0')}
+      >
+        <BsWhatsapp size='24px' />
+      </Button >
+    </Tooltip>
   )
 }
