bbaa4449777db00dbf29601b10cf070c0eb25246
Alexis Pavon <themaster034@gmail.com>
2023-01-04 19:50:06 -0300

    add color selected

--- components/Products/ColorSelected.tsx
+++ components/Products/ColorSelected.tsx
@@ -1,32 +1,59 @@
 import { Box, Stack } from "@chakra-ui/react"
+import { groq } from "next-sanity";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+import { sanityClient } from "../../sanity";
 
 interface ColorSelectedProps {
-  setColorSelected: (value: number) => void
-  colorSelected: number
+  color: Colors;
+  slug: string;
 }
 
-export const ColorSelected = ({ colorSelected, setColorSelected }: ColorSelectedProps) => {
-  const colors = ['red', 'orange', 'green', 'brown', 'aqua', 'black']
+interface Colors {
+  url: string
+}
+
+export const ColorSelected = () => {
+  const router = useRouter()
+
+  const { data = [], error } = useSWR(
+    groq`
+    *[_type == 'products']{
+      color{
+        ...asset -> {url}
+      },
+        "slug": slug.current
+    }`,
+    (query) => sanityClient.fetch(query));
+
   return (
     <Stack m='10px 0' spacing={2} direction='row' align='center'>
-      {colors.map((_, i) => (
-        <Box
-          key={i}
-          onClick={() => setColorSelected(i)}
-          transition='all .3s ease'
-          rounded='full'
-          h='30px'
-          w='30px'
-          bg={_}
-          style={
-            colorSelected == i ? {
-              transform: 'scale(0.85)',
-              boxShadow: '0 0 0 1px black',
-              border: '2px solid white'
-            } : {}
-          }
-        />
-      ))}
+      {data?.map(({ color, slug }: ColorSelectedProps, i: number) => {
+        if (!color || Object.keys(color).length === 0) return;
+        return (
+          <NextLink key={i} href={`/product/${slug}`}>
+            <Box
+
+              transition='all .3s ease'
+              rounded='full'
+              h='30px'
+              w='30px'
+              backgroundImage={color?.url}
+              backgroundSize='30px'
+              style={
+                router.query.slug == slug ? {
+                  transform: 'scale(0.85)',
+                  boxShadow: '0 0 0 1px black',
+                  border: '2px solid white'
+                } : {}
+              }
+            />
+          </NextLink>
+        )
+      }
+      )}
     </Stack>
   )
 }
