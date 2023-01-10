eb2417214d2e5a032c2da921fa390e568637a150
Alexis Pavon <themaster034@gmail.com>
2023-01-10 17:35:49 -0300

    add coupons and shipping

--- components/CheckoutOrder/CheckoutOrder.tsx
+++ components/CheckoutOrder/CheckoutOrder.tsx
@@ -1,12 +1,60 @@
-import { Box, Button, Divider, Flex, Input, Text } from '@chakra-ui/react'
-import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  Text
+} from '@chakra-ui/react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useForm } from 'react-hook-form'
+import { groq } from 'next-sanity'
 
-import { ICartProduct } from '../../interfaces'
-import { getCartTotal } from '../../redux/cart/cartSlices'
+import { ICartProduct, IProduct } from '../../interfaces'
+import { addToCart, getCartTotal } from '../../redux/cart/cartSlices'
+import { sanityClient } from '../../sanity'
 
 export const CheckoutOrder = () => {
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  const { register, handleSubmit, setValue, setError, formState: { errors } } = useForm()
   const { items } = useSelector((state: any) => state.cart)
+  const shipping = useSelector((state: any) => state.shippings)
   const taxRate = (Number(process.env.NEXT_PUBLIC_TAX_RATE || 0) + 1)
+
+  const getCouponsCode = async (data: any) => {
+    setLoading(true)
+    try {
+      const query = groq`
+      *[_type == 'coupons'][] {
+        couponNumber,
+        discount
+      }   
+    `
+      const coupons = await sanityClient.fetch(query)
+      const result = coupons.find(({ couponNumber }: { couponNumber: string }) => couponNumber == data.couponCode)
+
+      if (!result) {
+        setError('couponCode', { message: 'coupon code is invalid' })
+        return
+      }
+
+      const newProductWhitCupon = items.map((item: IProduct) => ({
+        ...item,
+        discountCode: result
+      }))
+
+      dispatch(addToCart(newProductWhitCupon))
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <Box p='40px'>
@@ -28,25 +76,37 @@ export const CheckoutOrder = () => {
               fontWeight='600'
               fontSize='14px'
             >
-              {product.title}
+              {product?.title}
             </Text>
             <Text fontSize='14px'>Quantity: {product.quantity}</Text>
-            <Text fontSize='14px'>Size: {product.size}</Text>
+            <Text fontSize='14px'>Top Size: <strong>{product.topSize}</strong></Text>
+            <Text fontSize='14px'>Bottom Size: <strong>{product.bottomSize}</strong></Text>
           </Box>
           <Box pl='14px'>
-            <Text>${product.price.toFixed(2)}</Text>
+            <Text>${product.price?.toFixed(2)}</Text>
           </Box>
         </Flex>
       ))}
       <Divider />
       <Flex margin='20px 0'>
-        <Input type='text' name='discount' focusBorderColor='none' />
-        <Button ml='12px'>
+        <FormControl isInvalid={!!errors.couponCode}>
+          <Input
+            type='text'
+            focusBorderColor='none'
+            {...register('couponCode', { required: 'coupon code is required' })}
+          />
+          <FormErrorMessage>{errors?.couponCode?.message as string}</FormErrorMessage>
+        </FormControl>
+        <Button
+          ml='12px'
+          isLoading={loading}
+          onClick={handleSubmit(getCouponsCode)}
+        >
           Apply
         </Button>
       </Flex>
       <Divider />
-      {/* <Flex direction='column' rowGap='10px' p='20px 0'>
+      <Flex direction='column' rowGap='10px' p='20px 0'>
         <Flex justifyContent='space-between'>
           <Text
             fontSize='14px'
@@ -60,29 +120,34 @@ export const CheckoutOrder = () => {
             ${getCartTotal(items)}
           </Text>
         </Flex>
-        <Flex justifyContent='space-between'>
-          <Text
-            fontSize='14px'
-          >
-            Tax
-          </Text>
-          <Text
-            fontSize='14px'
-            fontWeight='600'
-          >
-            {`$${Number(process.env.NEXT_PUBLIC_TAX_RATE || 0).toFixed(2)}`}
-          </Text>
-        </Flex>
+        {
+          items[0]?.discountCode
+            ? <Flex justifyContent='space-between'>
+              <Text
+                fontSize='14px'
+              >
+                {`Discount (${items[0]?.discountCode.discount}%)`}
+              </Text>
+              <Text
+                fontSize='14px'
+                fontWeight='600'
+              >
+                {`$${((Number(items[0]?.discountCode.discount) / 100) * getCartTotal(items))}`}
+              </Text>
+            </Flex>
+            : <></>
+        }
+
         <Flex justifyContent='space-between'>
           <Text>Shipping</Text>
           <Text
             fontSize='14px'
             fontWeight='600'
           >
-            $0.00
+            {`$${shipping?.price}`}
           </Text>
         </Flex>
-      </Flex> */}
+      </Flex>
       <Divider />
       <Flex justifyContent='space-between' mt='12px'>
         <Text
@@ -99,7 +164,7 @@ export const CheckoutOrder = () => {
             fontSize='24px'
             fontWeight='600'
           >
-            ${(getCartTotal(items) * taxRate)}
+            ${(getCartTotal(items, items[0]?.discountCode?.discount) * taxRate) + shipping.price}
           </Text>
         </Flex>
       </Flex>
