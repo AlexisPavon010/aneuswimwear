11c4e75976f65ce4b6dcee4d302fb1b2498e8965
Alexis Pavon <themaster034@gmail.com>
2023-01-10 17:26:46 -0300

    remove typings

--- typings.d.ts
+++ typings.d.ts
@@ -2,13 +2,3 @@ declare module '*.html' {
   const value: any;
   export default value;
 }
-
-declare module 'react-rating-stars-component' {
-  const value: any
-  export default value
-}
-
-declare module 'react-easy-crop' {
-  const value: any
-  export default value
-}
