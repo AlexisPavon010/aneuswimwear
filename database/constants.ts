f8ba43e44f61de5079e369a9a4aa7a3ac17d1bca
Alexis Pavon <themaster034@gmail.com>
2023-01-10 17:37:38 -0300

    add constanst

--- database/constants.ts
+++ database/constants.ts
@@ -1,5 +1,6 @@
 export const SHOP_CONTANST = {
   validGeenders: ['men', 'women', 'kid', 'unisex', 'best_sellers', 'news'],
-  validTags: ['top', 'bottom', 'one-pices', 'new', 'new-bottom', 'best_sellers', 'all-swim', 'sale', 'one-pieces'],
+  validTags: ['top', 'bottom', 'one pieces', 'new', 'new-bottom', 'best_sellers', 'all-swim', 'sale', 'one-pieces'],
+  validCollections: ['new', 'all-swim', 'sale'],
   validSize: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 }
