5fab9a46a1f58c0519c8a268a3d8424f861a6daf
Alexis Pavon <themaster034@gmail.com>
2023-01-10 17:48:10 -0300

    remove admin page

--- client/index.ts
+++ client/index.ts
@@ -1,7 +1,4 @@
-export { postReview } from "./review";
 export { Payphone } from "./payphone";
 export { createOrder } from "./order";
-export { uploadBanner } from "./banner";
 export { uploadNewsletter, sendNewsletterEmail } from "./newsletter";
 export { registerUser, updateUser, recoveryPassword, resetPassword } from "./user";
-export { createProduct, deleteFiles, updateProduct, uploadFiles } from "./product";
