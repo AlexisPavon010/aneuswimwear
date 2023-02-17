ffdbbb411963c65f82cd0b0fc52bd161a43e4c32
Alexis Pavon <themaster034@gmail.com>
2023-02-17 12:34:58 -0300

    add sucess order client api

--- client/order.ts
+++ client/order.ts
@@ -6,6 +6,9 @@ export const createOrder = (payload: IOrder) => {
   return axios.post(`/api/order`, payload)
 }
 
-export const successOrder = (payload: IOrder) => {
-  return axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/email/success`, payload)
+export const successOrder = (payload: IOrder, email: string) => {
+  return axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/email/success`, {
+    payload,
+    email
+  })
 }
