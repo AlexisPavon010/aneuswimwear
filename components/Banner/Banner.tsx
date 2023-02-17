9d893fba043b6e95a47de686a115e218fe41eb52
Alexis Pavon <themaster034@gmail.com>
2023-02-17 12:35:12 -0300

    add button

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,6 +1,8 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay } from "swiper";
 import NextImage from 'next/image';
+import { Button, Flex } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 interface banner {
   images: Image[];
@@ -39,6 +41,35 @@ export const Banner = ({ banner }: { banner: banner }) => {
           width={1000}
           height={1000}
         />
+        <Flex
+          position='absolute'
+          zIndex=' 3'
+          left=' 0'
+          top=' 0'
+          bottom=' 0'
+          flex=' 1'
+          right=' 0'
+          font-size=' 1'
+          display=' flex'
+          width=' 100%'
+          justifyContent=' center'
+          alignItems='center'
+        >
+          <Button
+            as={NextLink}
+            href='/collections/all'
+            variant='outline'
+            color='white'
+            borderRadius={0}
+            _hover={{}}
+            fontSize='14px'
+            fontWeight={400}
+            height='43px'
+            width='150px'
+          >
+            SHOP NOW
+          </Button>
+        </Flex>
       </SwiperSlide>
     </Swiper>
   )
