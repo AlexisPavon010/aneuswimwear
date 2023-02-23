9d7075b50ac626cdefaa18331e1578816477f637
Alexis Pavon <themaster034@gmail.com>
2023-02-22 21:20:55 -0300

    remove update newsletter

--- client/index.ts
+++ client/index.ts
@@ -1,4 +1,4 @@
 export { Payphone } from "./payphone";
 export { createOrder } from "./order";
-export { uploadNewsletter, sendNewsletterEmail } from "./newsletter";
+export { sendNewsletterEmail } from "./newsletter";
 export { registerUser, updateUser, recoveryPassword, resetPassword } from "./user";
--- client/newsletter.ts
+++ client/newsletter.ts
@@ -1,18 +1,5 @@
 import axios from 'axios'
 
-interface Payload {
-  description: string;
-  firstTitle: string;
-  image: string[];
-  secondTitle: string;
-  subtitle: string;
-  _id: string;
-}
-
-export const uploadNewsletter = (payload: Payload) => {
-  return axios.post('/api/admin/newsletter', payload)
-}
-
 export const sendNewsletterEmail = (email: string) => {
   return axios.post('/api/email/newsletter', {
     email
