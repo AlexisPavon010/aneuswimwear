b53378f2fb307f0345f57df9c03614818d030cab
Alexis Pavon <themaster034@gmail.com>
2023-03-16 00:51:37 -0300

    fix one pieces

--- database/constants.ts
+++ database/constants.ts
@@ -1,7 +1,7 @@
 export const SHOP_CONTANST = {
   validGeenders: ['swimwear', 'sale', 'best_sellers', 'new', 'all-swim'],
-  validTypes: ['top', 'bottom', 'one-pieces'],
-  validTags: ['tops', 'bottoms', 'one-pieces'],
+  validTypes: ['top', 'bottom', 'one_pieces'],
+  validTags: ['tops', 'bottoms', 'one_pieces'],
   validCollections: ['new', 'swimwear', 'sale'],
   validSize: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 }
