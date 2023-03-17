b2acdf04b122b85c9cf421af06ed96bb13133682
Alexis Pavon <themaster034@gmail.com>
2023-03-17 18:09:56 -0300

    3

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,12 +39,12 @@ export default function Home({ news, best_sellers, banner, newsletter }: HomePro
       <Box as='main'>
         <Banner banner={banner} />
         <Container maxW='1600px'>
-          {/* <SlideProducts title='NEW ARRIVALS' products={news} />
-          <Categories />
-          <SlideProducts title='BEST SELLERS' products={best_sellers} />
-          <InstaFeed />
-          <WhatsAppButton />
-          <Newsletter newsletter={newsletter} /> */}
+          <SlideProducts title='NEW ARRIVALS' products={news} />
+          {/* <Categories /> */}
+          {/* <SlideProducts title='BEST SELLERS' products={best_sellers} /> */}
+          {/* <InstaFeed /> */}
+          {/* <WhatsAppButton /> */}
+          {/* <Newsletter newsletter={newsletter} /> */}
         </Container>
       </Box>
     </>
